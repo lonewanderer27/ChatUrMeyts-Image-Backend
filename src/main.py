c907305ffd0799f8294894580a1a1c7b62e1c460
lonewanderer27 <adrianejames27@gmail.com>
2024-11-26 09:22:55 +0800

    Rename API title and description for clarity

--- src/main.py
+++ src/main.py
@@ -10,8 +10,8 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 chaturmeytsimg = FastAPI(
-    title="Chat-Ur-Meyts OCR API",
-    description="API for Chat-Ur-Meyts OCR",
+    title="Chat-Ur-Meyts Image API",
+    description="API for Chat-Ur-Meyts Image",
     version="1.0"
 )
 
